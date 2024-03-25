86b729872529bcaf80efaa07472423fcd72623b5
SuelenNasc <suh_nass@hotmail.com>
2024-03-24 23:23:11 -0300

    Ex06

---
+++ Ex06.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex06 {
+    public static void executar () {
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.print("Digite a temperatura em graus Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        double fahrenheit = (9 * celsius + 160) / 5; 
+
+      
+        System.out.println("A temperatura em Fahrenheit é: " + fahrenheit);
+
+        scanner.close();
+    }
+}
+
